fc5d33134bbca2819e46e6d85b5cae2923e773d1
arian-jtj <arian.jtj@gmail.com>
2025-02-22 00:21:47 +0700

    Updated Minigame
    SOMEHOW THIS TAKES 9 HOURS AND IT'S STILL NOT WORKING

--- GCBG GIM JAM 2025/Assets/Script/Minigame/GridController.cs
+++ GCBG GIM JAM 2025/Assets/Script/Minigame/GridController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Video;
 
 public class GridController : MonoBehaviour, IDropHandler
 {
@@ -8,23 +9,39 @@ public class GridController : MonoBehaviour, IDropHandler
     public string possibility2;
     public string possibility3;
 
-    public string detectedPieceInGrid;
+    [Header("Grid Properties [Leave Empty]")]
+    public string detectedPieceInGrid = "0";
+
+    public bool isGridEmpty = true;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        isGridEmpty = false;
+        detectedPieceInGrid = collision.GetComponent<PieceController>().pieceName;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        isGridEmpty = true;
+        detectedPieceInGrid = "0";
+    }
 
     // Dropping anything from pointer
     public void OnDrop(PointerEventData eventData)
     {
-        // If the pointer is dragging something
+        // If the pointer is dropping something
         if(eventData.pointerDrag != null)
         {
-            // If it's the correct piece
-            if(eventData.pointerDrag.GetComponent<PieceController>().pieceName == possibility1 || eventData.pointerDrag.GetComponent<PieceController>().pieceName == possibility2 || eventData.pointerDrag.GetComponent<PieceController>().pieceName == possibility3)
-            {
+            // If it's the correct piece that is dropped
+            // if(eventData.pointerDrag.GetComponent<PieceController>().pieceName == possibility1 || eventData.pointerDrag.GetComponent<PieceController>().pieceName == possibility2 || eventData.pointerDrag.GetComponent<PieceController>().pieceName == possibility3)
+            // {
                 detectedPieceInGrid = eventData.pointerDrag.GetComponent<PieceController>().pieceName;
-            }
-            else
-            {
-                // Debug.Log("False");
-            }
+                // Debug.Log(detectedPieceInGrid);
+            // }
+            // else
+            // {
+            //     // Debug.Log("False");
+            // }
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
         }
     }
