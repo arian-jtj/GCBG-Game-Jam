d52d55ab9b5126066c9d4537cec2f0658cda938c
Rizza Dzalfa Septananda <rizzasyabia@gmail.com>
2025-02-24 21:59:20 +0700

    Fix  and polish Level 3

--- GCBG GIM JAM 2025/Assets/Script/Movement Morgan.cs
+++ GCBG GIM JAM 2025/Assets/Script/Movement Morgan.cs
@@ -2,37 +2,50 @@ using UnityEngine;
 
 public class MovementMorgan : MonoBehaviour
 {
+    public PlayerInventoryValue item;
     Vector2 _movement;
     [SerializeField]
     Rigidbody2D _rigidBody;
 
     float _speed = 5f;
-
     [SerializeField]
     Animator _animator;
     Vector2 _lastDirection;
 
+    bool canMove = true;
+    bool isMonologue = false;
+
     void Update()
     {
+        StopMoveDialogue();
         HandleInput();
         HandleAnimation();
     }
+
     void FixedUpdate()
     {
-        _rigidBody.position = _rigidBody.position + _movement * _speed * Time.fixedDeltaTime;
-    } 
+        if (canMove)
+        {
+            _rigidBody.MovePosition(_rigidBody.position + _movement * _speed * Time.fixedDeltaTime);
+        }
+    }
 
     void HandleInput()
     {
+        if (!canMove)
+        {
+            _movement = Vector2.zero;
+            return;
+        }
+
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
         if (horizontalMovement == 0 && _movement.x != 0)
         {
             _lastDirection = _movement;
         }
         _movement = new Vector2(horizontalMovement, 0);
-
-
     }
+
     void HandleAnimation()
     {
         _animator.SetFloat("Square Magnitude", _movement.sqrMagnitude);
@@ -41,6 +54,40 @@ public class MovementMorgan : MonoBehaviour
         {
             _animator.SetFloat("Last Direction", _lastDirection.x);
         }
+    }
 
+    void StopMoveDialogue()
+    {
+        if (DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            canMove = isMonologue;
+        }
+        else
+        {
+            canMove = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("NPCDialogue"))
+        {
+            isMonologue = false;
+            canMove = false;
+        }
+        else if (collider.CompareTag("Monologue"))
+        {
+            isMonologue = true;
+            canMove = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("NPCDialogue") || collider.CompareTag("Monologue"))
+        {
+            isMonologue = false;
+            canMove = true;
+        }
     }
 }
